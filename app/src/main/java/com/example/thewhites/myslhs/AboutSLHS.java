a4868268dad7d6a47b54d56278f02a6fe0a2469b
tytanium9 <twhite@slhs.us>
2015-08-22 18:51:51 -0500

    Added a test comment to see if this actually works.

--- app/src/main/java/com/example/thewhites/myslhs/AboutSLHS.java
+++ app/src/main/java/com/example/thewhites/myslhs/AboutSLHS.java
@@ -13,7 +13,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class AboutSLHS extends Activity {
-
+//This is a test comment
     ImageView mShorelandLogo;
     TextView mSubStatement;
     Button mMissionVision;
